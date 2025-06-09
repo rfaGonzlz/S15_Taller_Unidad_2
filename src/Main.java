b127f84c5052c96f5d6c267fc7da330754290f07
rfaGonzlz <rafael.gonzalez@est.iudigital.edu.co>
2025-06-08 22:32:21 -0500

    feat: invocar métodos para agregar e imprimir animales.

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Se crea una instance de la clase Animales.
+        Animales animales = new Animales();
+
+
+// Se llama al método para agregar animales.
+        animales.agregarAnimal();
+// Se llama al método para imprimir los animales agregados.
+        animales.imprimirAnimales();
+
+
     }
 }
